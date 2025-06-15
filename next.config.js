0fc4ee8be65518a6dd322e4723ce7967f40d1dfe
ammar <ammarshaban5@gmail.com>
2025-06-15 11:07:08 +0300

    final

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // experimental: {
+  //   ppr: 'incremental', ❌ REMOVE THIS
+  // },
+};
+
+module.exports = nextConfig;
